2c5f52b60b7577c38ebf47481421d28454c5df9c
Timofey Busarev <busarev05@inbox.ru>
2024-01-05 21:32:39 +0300

    add seminar1: task2

---
+++ seminar1/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого числа в степень, 
+// равную третьеё цифре.
+
+int num = 678;
+num = Math.Abs(num);
+int first_dig = (num % 100) / 10;
+int second_dig = num % 10;
+int pow = 1;
+for (int i = 1; i <= second_dig; i++)
+{
+	pow = pow * second_dig;
+}
+Console.Write(pow);
+
