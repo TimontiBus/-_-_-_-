341b198ca6a1ea6d2ea1bbdab49cfd996ce6aed7
Timofey Busarev <busarev05@inbox.ru>
2024-01-05 21:32:00 +0300

    add seminar1: task1

---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите прорамму, которая принимает на вход трёхзначное число
+// и удаляет вторую цифру этого числа
+
+int num = 345;
+num = Math.Abs(num);
+int first_dig = num / 100;
+int second_dig = num % 10;
+Console.Write($"{first_dig}{second_dig}");
