cf03897a38004ff501f73970f0e290494b673c36
Timofey Busarev <busarev05@inbox.ru>
2024-01-09 23:24:56 +0300

    add Task4. Поиск максимального числа из массива (гири)

---
+++ lesson2/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] array = { 3, 5, 7, 67, 25, 300, 1345, 32, 654, 10 };
+int i = 0;
+int max = array[0];
+
+while (i < array.Length)
+{
+	if (array[i] > max)
+	{
+		max = array[i];
+	}
+	i = i + 1;
+}
+Console.Write(max);
