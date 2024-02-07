c2623936e5e834d0f7e07f3b94c925a504758c0c
Timofey Busarev <busarev05@inbox.ru>
2024-02-07 17:10:04 +0300

    add task3 с решением в ds_sem2

---
+++ ds_sem2/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] array = {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01, 34.876};
+
+double min = array[0];
+double max = array[1];
+double sum = 0;
+for(int i = 0; i<array.Length; i++)
+{
+    if(array[i]<min)
+    {
+        min = array[i]; 
+    }
+    if(array[i]>max)
+    {
+        max = array[i];
+    }
+}
+sum = max - min;
+Console.Write(sum);
+
+Console.Clear();
