1458ccaf8dfc659e4557e2a0bee1713b5d240c43
Timofey Busarev <busarev05@inbox.ru>
2024-04-14 16:19:11 +0300

    add ds_sem3 3 задачи

--- ds_sem3/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ ds_sem3/task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b075e9c1ca9cfa443ee69ab42d6067e1aea3afb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2082633d878ae3a47338055311be6ad47576d0fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ds_sem3/task3/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+
+
+// Заполните массив на N (вводится с консоли, не более 8) случайных целых чисел от 0 до 9. 
+// Сформируйте целое число, которое будет состоять из цифр из массива. 
+// Старший разряд числа находится на 0-м индексе, младший – на последнем.
+
+
+using System;
+class Program
+{
+	static void Main()
+	{
+		int[] numbers = { 1, 3, 5, 6, 7, 8 }; // Исходный массив
+		int temp;
+		// Вывод исходного массива
+		Console.Write("Исходный массив: ");
+		foreach (int number in numbers)
+		{
+			Console.Write(number + " ");
+		}
+		// Реверсирование массива
+		for (int i = 0; i < numbers.Length / 2; i++)
+		{
+			// Меняем местами элементы
+			temp = numbers[i];
+			numbers[i] = numbers[numbers.Length - 1 - i];
+			numbers[numbers.Length - 1 - i] = temp;
+		}
+		// Вывод измененного массива
+		Console.Write("\nПеревернутый массив: ");
+		foreach (int number in numbers)
+		{
+			Console.Write(number + " ");
+		}
+	}
+}
