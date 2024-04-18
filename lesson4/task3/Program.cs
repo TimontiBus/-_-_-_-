7cdfa7ad2c854fe0c3e1f28d60ed61968516adf4
Timofey Busarev <busarev05@inbox.ru>
2024-04-18 21:08:56 +0300

    add task3 строка с цифрами, только буквы

---
+++ lesson4/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// При вводе слова с цифрами, код выводит только буквы.
+
+
+string GetLettersFromString(string s)
+{
+	string letters = "";
+	foreach (char e in s)
+	{
+		if (char.IsAsciiLetter(e) == true)
+		{
+			letters = letters + e;
+		}
+	}
+	return letters;
+}
+
+string str = Console.ReadLine();
+string resull = GetLettersFromString(str);
+Console.WriteLine(resull);
