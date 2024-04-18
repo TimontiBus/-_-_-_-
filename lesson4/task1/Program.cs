4bd8f48a16066b7a6867d8bb221d3b73e551eae5
Timofey Busarev <busarev05@inbox.ru>
2024-04-18 20:07:08 +0300

    add lesson4, task1

---
+++ lesson4/task1/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+// int[,] matrix = new int[3, 5];
+
+// Random rnd = new Random();
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// 	for (int j = 0; j < matrix.GetLength(1); j++)
+// 	{
+// 		matrix[i, j] = rnd.Next(1, 11);
+// 	}
+// }
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// 	for (int j = 0; j < matrix.GetLength(1); j++)
+// 	{
+// 		Console.Write($"{matrix[i, j]} ");
+// 	}
+// 	Console.WriteLine(); // Добавлен переход на новую строку после каждой строки матрицы
+// }
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+	int[,] matrix = new int[rowCount, columsCount];
+
+	Random rnd = new Random();
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			matrix[i, j] = rnd.Next(1, 11);
+		}
+	}
+	return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+	for (int i = 0; i < matrix.GetLength(0); i++)
+	{
+		for (int j = 0; j < matrix.GetLength(1); j++)
+		{
+			Console.Write($"{matrix[i, j]} ");
+		}
+		Console.WriteLine(); // Добавлен переход на новую строку после каждой строки матрицы
+	}
+}
+
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
