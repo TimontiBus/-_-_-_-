5c0b919031d6f517a498ba716687bf6dc839fbf6
Timofey Busarev <busarev05@inbox.ru>
2024-04-18 20:51:26 +0300

    add task2

---
+++ lesson4/task2/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int[,] CreateMatrix(int rowCount, int columsCount)
+		{
+			int[,] matrix = new int[rowCount, columsCount];
+
+			Random rnd = new Random();
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					matrix[i, j] = rnd.Next(1, 1000);
+				}
+			}
+			return matrix;
+		}
+
+		void ShowMatrix(int[,] matrix)
+		{
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					Console.Write($"{matrix[i, j]} ");
+				}
+				Console.WriteLine(); // Добавлен переход на новую строку после каждой строки матрицы
+			}
+		}
+
+		int[,] matrix = CreateMatrix(4, 4);
+		ShowMatrix(matrix);
+		foreach (int e in matrix)
+		{
+			if (isInteresting(e))
+			{
+				Console.WriteLine(e);
+			}
+		}
+
+		bool isInteresting(int value)
+		{
+			int sumOfDigits = GetSumOfDigits(value);
+			if (sumOfDigits % 2 == 0)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		int GetSumOfDigits(int value)
+		{
+			int sum = 0;
+			while (value > 0)
+			{
+				sum = sum + value % 10;
+				value = value / 10;
+			}
+			return sum;
+		}
+	}
+}
