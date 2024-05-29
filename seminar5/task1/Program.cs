321bb1e61d811a768e0e7d0f808f05839e510827
Timofey Busarev <busarev05@inbox.ru>
2024-05-29 17:33:53 +0300

    add seminar 5 массивы и строки

---
+++ seminar5/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// char ch = 'E';
+// char ch2 = 'r';
+// char ch3 = 'r';
+// Console.WriteLine(ch);
+// Console.WriteLine(ch2);
+// Console.WriteLine(ch3);
+
+
+
+// char[] ch4 = { '1', '2', '$', '%' };
+// Console.WriteLine(ch4);
+
+// Задайте массив символов (тип char []). Создайте строку из символов этого массива.
+
+char[] ch = { 'a', 'b', ';', 'd' };
+string str = "";
+foreach (char e in ch)
+{
+	str += e;
+}
+
+Console.WriteLine(str);
