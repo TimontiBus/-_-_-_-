d61e1dcdbc5338bf292f2cf4587ed9c1e2cd0703
Timofey Busarev <busarev05@inbox.ru>
2024-06-08 12:37:18 +0300

    add sem5

---
+++ seminar5/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// На оснвое симоволов строки (тип string) сформировать массив символов (тип char[]).
+
+string str = "Hellow!";
+char[] ch = new char[str.Length];
+for (int i = 0; i < str.Length; i++)
+{
+	ch[i] = str[i];
+}
+foreach (char e in ch)
+{
+	Console.WriteLine(e + " ");
+}
